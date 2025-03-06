e96f98fe71878b7fa7cddd13f16ea2ed23ff5a26
Kirill <2vvoropaevv2@gmail.com>
2025-03-06 17:10:53 +0700

    Add Example work

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
-from concurrent.futures import ThreadPoolExecutor
+import json
 
-import requests
 from celery import group
 
 from services.work_with_pages_service.async_pages_process import get_pages, get_xml_files
@@ -9,17 +8,18 @@ from services.work_with_pages_service.tasks import take_links, parse_xml
 
 
 def save_result(data):
-    with open("result.txt", "w") as file:
-        for tup in data:
-            file.write(str(tup) + "\n")
+    with open("result.json", "w") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 def main():
+    ## TODO Some queries return a 404 error, so some tenders may not be found
     pages_html = asyncio.run(get_pages())
     job = group([
         take_links.s(page) for page in pages_html
     ])
     pages_links = job.apply_async().get()
 
+    ## TODO Some queries return a 404 error, so some tenders may not be found
     xml_files = asyncio.run(get_xml_files(pages_links))
     job = group([
         parse_xml.s(link, xml_file) for link, xml_file in xml_files
